3b7940ee677efa948d7ee3fd92d60160c391d0d8
HyperMutekiXXX <1072183829@qq.com>
2023-08-08 18:24:57 +0800

    feat: 增加笔记 & lc1071

--- leetcode/lc1071/lc1071_test.go
+++ leetcode/lc1071/lc1071_test.go
@@ -1,1 +1,31 @@
 package lc1071
+
+import (
+	"fmt"
+	"testing"
+)
+
+func gcdOfStrings(str1 string, str2 string) string {
+	tmp1 := str1 + str2
+	tmp2 := str2 + str1
+	if tmp1 != tmp2 {
+		return ""
+	}
+	return str1[:gcd(len(str1), len(str2))]
+}
+
+func gcd(a, b int) int {
+	remainder := a % b
+	for remainder != 0 {
+		a = b
+		b = remainder
+		remainder = a % b
+	}
+	return b
+}
+
+func TestGcd(t *testing.T) {
+	str1 := "ABABAB"
+	str2 := "ABAB"
+	fmt.Println(gcdOfStrings(str1, str2))
+}
